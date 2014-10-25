bff2800174494e9ebb66de364f2137ec01733976
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-25 16:29:41 +0100

    Added new if statements concerning fractions and whole numbers

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -6,14 +6,24 @@ public class FractionCalculator {
 			if (splitNumbers[count].equals("/" || "+" || "-" || "+") {
 
 
-			} else if (splitNumbers[count].contains("/")) {
+			} else if (splitNumbers[count].equals("a" || "A" || "abs") {
+
+			} else if (splitNumbers[count].equals("n" || "N" || "neg") {
+
+			} else if (splitNumbers[count].equals("c" || "C" || "clear") {
+
+			} else if (splitNumbers[count].contains{
 				for (int i = 0; i < splitNumbers[count].length; i++) {
 					if (splitNumbers[count].charAt(i).equals("/") {
 						int num = Integer.parseInt(splitNumbers[count].substring(0, i));
 						int denom = Integer.parseInt(splitNumbers[count].substring(i+1, splitNumbers[count].length -1);
 						fraction.numerator = num;
 						fraction.denominator = denom;
-					}
+					} else {
+						int num = Integer.parseInt(splitNumbers[count]);
+						fraction.numerator = num;
+						fraction.denominator = 1;
 				}
 
-	public Fraction operation(String[] splitNumbers, Fraction fraction) {
+	public Fraction operation(String[] splitNumbers, int count) {
+		if (splitNumber[count].equals("+")
