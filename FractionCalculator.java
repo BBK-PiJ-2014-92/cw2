1479431d7bb18b465fcb7db4ef340c21f00a8a0b
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-25 17:15:43 +0100

    Added new method tempFraction

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -2,28 +2,41 @@ public class FractionCalculator {
 
 	public Fraction evaluate(Fraction fraction, String inputString) {
 		String[] splitNumbers = inputString.split(" ");
+		String rememberedOp = null;
 		for (int count = 0; count < splitNumbers.length; count++) {
 			if (splitNumbers[count].equals("/" || "+" || "-" || "+") {
-
-
+				rememberedOp = splitNumbers[count];
 			} else if (splitNumbers[count].equals("a" || "A" || "abs") {
 
 			} else if (splitNumbers[count].equals("n" || "N" || "neg") {
 
 			} else if (splitNumbers[count].equals("c" || "C" || "clear") {
 
-			} else if (splitNumbers[count].contains{
-				for (int i = 0; i < splitNumbers[count].length; i++) {
-					if (splitNumbers[count].charAt(i).equals("/") {
-						int num = Integer.parseInt(splitNumbers[count].substring(0, i));
-						int denom = Integer.parseInt(splitNumbers[count].substring(i+1, splitNumbers[count].length -1);
-						fraction.numerator = num;
-						fraction.denominator = denom;
-					} else {
-						int num = Integer.parseInt(splitNumbers[count]);
-						fraction.numerator = num;
-						fraction.denominator = 1;
+			} else if (splitNumbers[count].contains{"/") {
+				if (rememberedOp = null) {
+					fraction = tempFraction (String[] splitNumbers, count);
+				} else {
+					if (rememberedOp.equals("+") {
+								fraction = fraction.add(new Fraction(num, denom));
+							}
+						}
+					}
 				}
 
-	public Fraction operation(String[] splitNumbers, int count) {
-		if (splitNumber[count].equals("+")
+	public Fraction tempFraction (String[] splitNumbers, int count) {
+		if (splitNumbers[count].charAt(i).equals("/") {
+			for (int i = 0; i < splitNumbers[count].length; i++) {
+				if (splitNumbers[count].charAt(i).equals("/") {
+					int num = Integer.parseInt(splitNumbers[count].substring(0, i));
+					int denom = Integer.parseInt(splitNumbers[count].substring(i+1, splitNumbers[count].length -1);
+				}
+			}
+			return new Fraction(num, denom);
+		}else {
+			int num = Integer.parseInt(splitNumbers[count])
+			int denom = 1;
+			return new Fraction(num, denom);
+		}
+	}
+
+	public Fraction operator
