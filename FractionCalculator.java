43174155d472f925ee98995998570628455faf00
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-25 15:55:46 +0100

    removed the initialisor and class fields. started on new method operation

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,23 +1,19 @@
 public class FractionCalculator {
-	Fraction  calcValue;
-	String operation;
-	
-	public FractionCalculator {
-		this.numerator = 0;
-		this.denominator = 1;
-		this.operation = null;
-	}
-	
+
 	public Fraction evaluate(Fraction fraction, String inputString) {
 		String[] splitNumbers = inputString.split(" ");
 		for (int count = 0; count < splitNumbers.length; count++) {
-			if (splitNumbers[count].equals("/")) {
-				operation = splitNumbers[count];
+			if (splitNumbers[count].equals("/" || "+" || "-" || "+") {
+
+
 			} else if (splitNumbers[count].contains("/")) {
 				for (int i = 0; i < splitNumbers[count].length; i++) {
-					
-				
-				
-		
-				
-				
+					if (splitNumbers[count].charAt(i).equals("/") {
+						int num = Integer.parseInt(splitNumbers[count].substring(0, i));
+						int denom = Integer.parseInt(splitNumbers[count].substring(i+1, splitNumbers[count].length -1);
+						fraction.numerator = num;
+						fraction.denominator = denom;
+					}
+				}
+
+	public Fraction operation(String[] splitNumbers, Fraction fraction) {
