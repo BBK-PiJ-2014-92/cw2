11286aabbe59e4b186ee78cbc3ab96264459ad7b
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-26 13:58:28 +0000

    Changed "exist" to "exit" on line 7

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -1,19 +1,20 @@
 public class FractionCalculatorTest {
 
 	public static void main(String[] args) {
-		new FractionCalculator fc = new FractionCalculator();
-		System.out.println("Welcome to my Fraction Calculator by Ahmed Almazidi");
-		System.out.println("Please enter a line of numbers to be evaluated. Type 'q' or 'Q' to exist this program");
+		FractionCalculator fc = new FractionCalculator();
+		System.out.println("Welcome to Fraction Calculator by Ahmed Almazidi");
+		System.out.println("Please enter a line of numbers to be evaluated. Numbers and operators must be separated by spaces");
+		System.out.println("Type 'q' or 'Q' to exit this program");
 		boolean finished = false;
-		new Fraction fraction = new Fraction(0, 1);
+		Fraction fraction = new Fraction(0, 1);
 		while (!finished){
 			String input = System.console().readLine();
-			if (input.contains('q') || input.contains('Q') || input.contains('quit') || input.contains('Quit')){
+			if (input.equals("q") || input.equals("Q") || input.equals("quit") || input.equals("Quit")){
 				System.out.println("Goodbye");
 				finished = true;
 			} else {
-				fraction = evaluate(fraction, input);
+				fraction = fc.evaluate(fraction, input);
 			}
 		}
 	}
-
+}
