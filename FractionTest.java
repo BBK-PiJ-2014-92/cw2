3298e04fd1de1e4d98a1976f29636e358cb8f3e9
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-26 17:51:01 +0000

    Added a new test for absolute values of negative whole numbers

--- FractionTest.java
+++ FractionTest.java
@@ -12,6 +12,7 @@ public class FractionTest {
 	Fraction h = new Fraction(3,5);
 	Fraction i = new Fraction(-1,2);
 	Fraction j = new Fraction(1,-2);
+	Fraction k = new Fraction(-1, 1);
 	if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
         // test equals
 	test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
@@ -26,6 +27,7 @@ public class FractionTest {
     System.out.println(h.subtract(f)); //Result should be 3/10
     System.out.println(i.absValue()); //Result should be 1/2
     System.out.println(j.absValue()); //Result should be 1/2
+    System.out.println(k.absValue()); //Result should be 1
     System.out.println(f.negate()); //Result should be -3/10
     System.out.println(j.negate()); //Result should be 1/2
     System.out.println(i.negate()); //Result should be 1/2
